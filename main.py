33d5c7ce1f2f1d426102df3f6163655ce988e670
Theyoyo09 <ydemonchaud@gmail.com>
2022-09-11 19:34:44 +0200

    changement importation

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import cv2
 import keras
-import numpy as np
-from PIL.ImageOps import grayscale
-from tensorflow.python.ops.image_ops_impl import rgb_to_grayscale
 
 Categories = ["cats", "dogs"]
 
 def prepare():
     Img_size = 150
-    img_array = cv2.imread('test1/test1/179.jpg')
+    img_array = cv2.imread('test1/test1/6162.jpg')
     new_array = cv2.resize(img_array, (Img_size, Img_size))
     return new_array.reshape(-1, Img_size, Img_size, 3)
 
--- model_générateur.py
+++ model_générateur.py
@@ -1,22 +1,9 @@
 # set the matplotlib backend so figures can be saved in the background
 import matplotlib
-
-from sklearn.preprocessing import LabelBinarizer
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-import cv2
 from keras import layers
 from keras import models
 from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
-
-from imutils import paths
-import matplotlib.pyplot as plt
-import numpy as np
-import argparse
-import random
-import pickle
-import os
 
 matplotlib.use("Agg")
 
